d5c9e3fca500e56b6e6c02d41b1db0cb94e5b062
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-04-14 15:06:15 -0300

    feat(tests) Unit tests
    Added unit tests to base price feature

---
+++ controllers/baseprice.js
@@ -0,0 +1,74 @@
+var _ = require('lodash');
+const DEFAULT_VALUE = 1000
+module.exports.calculate = function(type ,year, model, make, incident) {
+    var basePrices = require('../config/basePrice.json');
+    var price = {
+        value: null,
+        priority: null
+    };
+    var car = {
+        "type":  type,  
+        "year": year,
+        "model": model,
+        "make": make,
+        "incidentAVG": incident
+    };
+    
+    basePrices.map(function(baseprice){
+        var item = _.clone(baseprice);
+        var basePrice = item.baseprice;        
+        delete item.baseprice;
+        var clonedCar = _.clone(car);
+        if(_.isEqual(item, clonedCar) && (_.isNull(price.value) || price.priority < 5)){
+            price = {
+                value : basePrice,
+                priority: 5
+            }
+            return;
+        }
+        
+        delete item.incidentAVG;
+        delete clonedCar.incidentAVG;
+        if(_.isEqual(item, clonedCar) && (_.isNull(price.value) || price.priority < 4)){
+            price = {
+                value : basePrice,
+                priority: 4
+            }
+            return;
+        }
+        
+        delete item.make;
+        delete clonedCar.make;
+        if(_.isEqual(item, clonedCar) && (_.isNull(price.value) || price.priority < 3)){
+            price = {
+                value : basePrice,
+                priority: 3
+            }
+            return;
+        }
+        
+        delete item.model;
+        delete clonedCar.model;
+        if(_.isEqual(item, clonedCar) && (_.isNull(price.value) || price.priority < 2)){
+            price = {
+                value : basePrice,
+                priority: 2
+            }
+            return;
+        }
+        
+        delete item.year;
+        delete clonedCar.year;
+        if(_.isEqual(item, clonedCar) && (_.isNull(price.value) || price.priority < 1)){
+            price = {
+                value : basePrice,
+                priority: 1
+            }
+            return;
+        }
+        
+    });
+
+    return price.value || DEFAULT_VALUE;
+
+};
---
+++ test/basePrice-test.js
@@ -0,0 +1,44 @@
+var test = require('tape');
+var controller = require('../controllers/basePrice.js');
+
+test('Base price all fields', function(t){
+    var basePrice = controller.calculate('other', 2013, 'unknown', 'unknown', 30);
+    var expectedBasePrice = 2500;
+
+    t.equal(basePrice, expectedBasePrice, 'Base price is: ' + basePrice + ' and should be: ' + expectedBasePrice);
+    t.end();
+}); 
+
+test('Base price fields (Type+Year+Make+Model)', function(t){
+    var basePrice = controller.calculate('motorcycle', 2013, 'k13000', 'BMW', 1);
+    var expectedBasePrice = 4000;
+
+    t.equal(basePrice, expectedBasePrice, 'Base price is: ' + basePrice + ' and should be: ' + expectedBasePrice);
+    t.end();
+}); 
+
+test('Base price fields (Type+Year+Make)', function(t){
+    var basePrice = controller.calculate('car', 2013, 'Uno', 'fiat', 22);
+    var expectedBasePrice = 3000;
+
+    t.equal(basePrice, expectedBasePrice, 'Base price is: ' + basePrice + ' and should be: ' + expectedBasePrice);
+    t.end();
+});
+ 
+test('Base price fields (Type+Year)', function(t){
+    t.plan(1);
+    var basePrice = controller.calculate('car', 2011, 'palio', 'fiat', 12);
+    var expectedBasePrice = 2000;
+
+    t.equal(basePrice, expectedBasePrice, 'Base price is: ' + basePrice + ' and should be: ' + expectedBasePrice);
+    t.end();
+});
+ 
+test('Base price fields (Type)', function(t){
+    t.plan(1);
+    var basePrice = controller.calculate('truck', 2011, '1120', 'mercedes', 12);
+    var expectedBasePrice = 8000;
+
+    t.equal(basePrice, expectedBasePrice, 'Base price is: ' + basePrice + ' and should be: ' + expectedBasePrice);
+    t.end();
+});
